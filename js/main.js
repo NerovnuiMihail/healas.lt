9d6b09f0cab88d46754d4b87748504cf7a7e7804
NerovnuiMihail <nerovnuimihail@gmail.com>
2024-02-11 15:03:56 +0300

    upd js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-const pricingCards = document.querySelectorAll('.pricing__card');
+// MOBILE MENU
+
+var burgerMenu = document.querySelector('.burger-menu');
+
+burgerMenu.addEventListener('click', (event) => {
+    burgerMenu.classList.toggle('burger-menu--active');
+    document.querySelector('.mobile-menu').classList.toggle('display-none');
+    document.body.classList.toggle('overflow-hidden');
+    document.querySelector('.header__logo').classList.toggle('hidden-logo');
+});
+
+document.querySelectorAll('.mobile-menu__wrapper > nav > ul > li > a').forEach(element => {
+    element.addEventListener('click', () => {
+        burgerMenu.classList.toggle('burger-menu--active');
+        document.querySelector('.mobile-menu').classList.toggle('display-none');
+        document.body.classList.toggle('overflow-hidden');
+        document.querySelector('.header__logo').classList.toggle('hidden-logo');
+    });
+});
+
+// PRICING CARDS
+
+var pricingCards = document.querySelectorAll('.pricing__card');
 
 pricingCards.forEach((card) => {
-    card.addEventListener('click', (event) => {
+    card.addEventListener('click', () => {
         pricingCards.forEach((card) => {
             card.classList.remove('pricing__card--active');
         });
